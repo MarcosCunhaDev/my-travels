ec82ab1fe5d742d3d3ba9bff61250cf94598403c
Marcoswrc2 <marcoswrc2@gmail.com>
2023-01-29 18:36:53 -0300

    Config: styled components

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+    }
+  }
 }
 
 module.exports = nextConfig
