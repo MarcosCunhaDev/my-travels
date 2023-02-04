194181d20714cd7270b3e34b43c1a7882ec119db
Marcoswrc2 <marcoswrc2@gmail.com>
2023-02-04 19:51:24 -0300

    Deleting unnecessary files

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,21 +1,5 @@
 import React from 'react'
-import * as S from './styles'
 
 export function Main() {
-  return (
-    <S.Wrapper>
-      <S.Logo
-        src="/img/logo.svg"
-        alt="Imagem de um átomo e React Avançado escrito ao lado."
-      />
-      <S.Title>React avançado</S.Title>
-      <S.Description>
-        TypeScript, ReactJS, NextJS e Styled Components
-      </S.Description>
-      <S.Illustration
-        src="/img/hero-illustration.svg"
-        alt="Desenvolvedor de frente para tela com código"
-      />
-    </S.Wrapper>
-  )
+  return <h1>NextJS na prática!</h1>
 }
