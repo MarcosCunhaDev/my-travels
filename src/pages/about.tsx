7bfe07493cd83707f2ebe1a535f337d0fc2ca63c
Marcoswrc2 <marcoswrc2@gmail.com>
2023-02-07 08:50:10 -0300

    Config: graphql

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,13 @@
 import { NextPage } from 'next'
 import About from '@/templates/About'
+import client from '@/graphql/client'
+import { GET_PAGES } from '@/graphql/queries'
+
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+  console.log(pages)
+  return { props: {} }
+}
 
 const AboutPage: NextPage = () => {
   return <About />
