72f8facd03d1eb96862c3cb9dae70f2b1c6b01cf
Marcoswrc2 <marcoswrc2@gmail.com>
2023-02-16 16:12:24 -0300

    Feat: add dynamic style to map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -12,6 +12,24 @@ export interface MapProps {
   places?: Place[]
 }
 
+const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY
+const MAPBOX_USERID = process.env.NEXT_PUBLIC_MAPBOX_USERID
+const MAPBOX_STYLEDID = process.env.NEXT_PUBLIC_MAPBOX_STYLEDID
+
+const CustomTileLayer = () => {
+  return MAPBOX_API_KEY ? (
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEDID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
+    />
+  ) : (
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  )
+}
+
 function Map({ places }: MapProps) {
   const router = useRouter()
 
@@ -21,10 +39,7 @@ function Map({ places }: MapProps) {
       zoom={2.5}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <CustomTileLayer />
       {places?.map(({ id, name, location, slug }) => {
         const { latitude, longitude } = location
         return (
